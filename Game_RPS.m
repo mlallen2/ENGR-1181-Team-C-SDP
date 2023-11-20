71e9022dd5463e7fbd3a6c8bb667e20a273b97fc
mlallen2 <99111424+mlallen2@users.noreply.github.com>
2023-11-20 12:13:59 -0500

    Update Game_RPS.m

--- Game_RPS.m
+++ Game_RPS.m
@@ -17,13 +17,14 @@ drawScene(rps_scene, [7, 7, 7, 7, 7, 7, 7; 8, 8, 8, 8, 8, 8, 8; 8, 4, 8, 8, 8, 5
 title('Rock Paper Scissors', 'FontSize',30)
 
 pause(1)
+
 % Assigns names to players
 txt_instructions = 'Enter Player 1''s name';
 obj1 = text(1750,2300,txt_instructions, 'fontsize',17,'Color','w','HorizontalAlignment', 'center'); 
 
 Name1 = input('Enter Player 1''s name', 's');
 txt_player1name = Name1;
-obj2 = text(750,1000,txt_player1name, 'fontsize',22,'Color','w' ,'HorizontalAlignment', 'center');
+obj2 = text(750,900,txt_player1name, 'fontsize',22,'Color','w' ,'HorizontalAlignment', 'center');
 
 delete(obj1);
 
@@ -32,24 +33,25 @@ obj1 =  text(1750,2300,txt_instructions, 'fontsize',17,'Color','w','HorizontalAl
 
 Name2 = input('Enter Player2''s name', 's');
 txt_player2name = Name2;
-obj3 = text(2750,1000,txt_player2name, 'fontsize',22,'Color','w','HorizontalAlignment', 'center');
+obj3 = text(2750,900,txt_player2name, 'fontsize',22,'Color','w','HorizontalAlignment', 'center');
 
 delete(obj1);
 
 % Assigins players their objects
 txt_instructions = [ Name1 , ' type your item: rock, paper, or scissors'];
-obj1 =  text(1750,2300,txt_instructions, 'fontsize',15,'Color','w','HorizontalAlignment', 'center');
+obj1 =  text(1750,2300,txt_instructions, 'fontsize',14,'Color','w','HorizontalAlignment', 'center');
 
 Player1_item = input('Enter rock, paper, or scissors  ');
 
 delete(obj1);
 
-txt_instructions = [ Name2 , ' type your item: rock, paper, or scissors'];
-obj1 = text(1750,2300,txt_instructions, 'fontsize',15,'Color','w','HorizontalAlignment', 'center');
+txt_instructions = [ Name2 , ' type your item: \n rock, paper, or scissors'];
+obj1 = text(1750,2300,txt_instructions, 'fontsize',14,'Color','w','HorizontalAlignment', 'center');
 
 Player2_item = input('Enter rock, paper, or scissors  ');
 
 delete(obj1);
+
 drawScene(rps_scene, [7, 7, 7, 7, 7, 7, 7;8, 8, 8, 8, 8, 8, 8; 8, 4, 8, 8, 8, 5, 8; 8, Player1_item, 8, 6, 8, Player2_item, 8; 8, 8, 8, 8, 8, 8, 8; 7, 7, 7, 7, 7, 7, 7])
 
 % Determines the reults of the game
@@ -67,12 +69,12 @@ drawScene(rps_scene, [7, 7, 7, 7, 7, 7, 7;8, 8, 8, 8, 8, 8, 8; 8, 4, 8, 8, 8, 5,
     end
 
 % Asks if they would like to play again   
-pause(2);
+pause(2.25);
 
 delete(obj1);
 
 txt_instructions = ('Type yes to continue, no to stop');
-obj1 = text(1750,2300,txt_instructions, 'fontsize',20,'Color','w','HorizontalAlignment', 'center');
+obj1 = text(1750,2300,txt_instructions, 'fontsize',16,'Color','w','HorizontalAlignment', 'center');
 playAgain = input('Type yes to continue, no to stop','s');
 
 yes = 0;
